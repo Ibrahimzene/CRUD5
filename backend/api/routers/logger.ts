2525883cdaacf9c84c6f2773eaddf1965f86569e
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2024-01-15 13:47:39 +0100

    Add winston logger and update dependencies

--- backend/api/routers/logger.ts
+++ backend/api/routers/logger.ts
@@ -1,6 +1,14 @@
+import e from 'express';
 import winston from 'winston';
+const { combine, timestamp, json } = winston.format;
 
 export const logger = winston.createLogger({
 	level: 'debug',
-	
+	format: combine(timestamp(), json()),
+	transports: [
+		new winston.transports.File({
+			filename: 'backend/api/logs/api.log',
+			level: 'debug'
+		})
+	]
 });
